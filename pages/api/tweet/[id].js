2006e3550f2a2ae9b1fb62a970879b31f008939a
Prince <thedrkprns@gmail.com>
2021-04-05 10:32:51 +0530

    possibly fixed inernal server error in prod

--- pages/api/tweet/[id].js
+++ pages/api/tweet/[id].js
@@ -1,12 +1,20 @@
 import axios from 'axios'
 
 const headers = {
-    'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
+    'Authorization': `Bearer ${process.env.NEXT_APP_BEARER_TOKEN}`
 }
+
+console.log(process.env.NEXT_APP_BEARER_TOKEN)
 
 export default async function handler (req, res) 
 {
 	const id = req.query.id
-	const data = await axios.get(`https://api.twitter.com/2/tweets/${id}?expansions=author_id,attachments.media_keys&user.fields=profile_image_url,verified&tweet.fields=created_at,attachments,public_metrics,entities,source&media.fields=preview_image_url,url`, {headers})
-	res.status(200).send({ data:  data.data})
+	try {
+		const response = await axios.get(`https://api.twitter.com/2/tweets/${id}?expansions=author_id,attachments.media_keys&user.fields=profile_image_url,verified&tweet.fields=created_at,attachments,public_metrics,entities,source&media.fields=preview_image_url,url`, {headers})
+		res.status(200).json({ data:  response.data, status: response.status})
+	}
+
+	catch(e) {
+		res.send({message: e})
+	}
 }
--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ function App() {
             e.preventDefault()
             const url = e.target.elements.tweetURL.value
             const id = url.split('/')[5]
-            const { data } = await axios.get(`/api/tweet/${id}`)
+            const { data, status } = await axios.get(`/api/tweet/${id}`)
             setLoading(false)
             setTweetData(data.data)
             setError(false)
