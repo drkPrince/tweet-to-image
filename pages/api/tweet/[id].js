9e91d4707295768ffab0a06ad96f3d03521f4f6d
Prince <thedrkprns@gmail.com>
2021-04-17 14:23:31 +0530

    trying fixing chrome android error

--- pages/api/tweet/[id].js
+++ pages/api/tweet/[id].js
@@ -4,13 +4,12 @@ const headers = {
     'Authorization': `Bearer ${process.env.NEXT_APP_BEARER_TOKEN}`
 }
 
-console.log(process.env.NEXT_APP_BEARER_TOKEN)
-
 export default async function handler (req, res) 
 {
 	const id = req.query.id
 	try {
 		const response = await axios.get(`https://api.twitter.com/2/tweets/${id}?expansions=author_id,attachments.media_keys&user.fields=profile_image_url,verified&tweet.fields=created_at,attachments,public_metrics,entities,source&media.fields=preview_image_url,url`, {headers})
+		console.log(response.data)
 		res.status(200).json({ data:  response.data, status: response.status})
 	}
 
